51a09ad857a7af703cae749c0648d3ffd0e3d069
laurarodriguez-growth <lnrsoto@gmail.com>
2026-07-27 15:15:27 -0500

    Add expose_headers to CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
 )
 
 
